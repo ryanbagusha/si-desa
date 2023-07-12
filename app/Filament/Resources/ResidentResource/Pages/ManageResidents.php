645bc2375eb5f848da98fd756cd5b1ce11a1e038
Ryan Bagus <ryanbagusha@gmail.com>
2023-07-12 18:25:00 +0800

    import data resident

--- app/Filament/Resources/ResidentResource/Pages/ManageResidents.php
+++ app/Filament/Resources/ResidentResource/Pages/ManageResidents.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Resources\ResidentResource\Pages;
 
-use App\Filament\Resources\ResidentResource;
+use Carbon\Carbon;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ManageRecords;
+use App\Filament\Resources\ResidentResource;
+use Konnco\FilamentImport\Actions\ImportField;
+use Konnco\FilamentImport\Actions\ImportAction;
+use Filament\Tables\Actions\Modal\Actions\Action;
 
 class ManageResidents extends ManageRecords
 {
@@ -14,8 +18,40 @@ class ManageResidents extends ManageRecords
     {
         return [
             Actions\CreateAction::make(),
-            Actions\Action::make('import')
-                ->color('secondary'),
+            ImportAction::make()
+                ->extraModalActions([
+                    Action::make('download_template')
+                        ->url(asset('/templates/template_import.csv'))
+                ])
+                ->color('secondary')
+                ->fields(
+                    [
+                        ImportField::make('nama')
+                            ->required(),
+                        ImportField::make('jenis_kelamin')
+                            ->required(),
+                        ImportField::make('tempat_lahir')
+                            ->required(),
+                        ImportField::make('tanggal_lahir')
+                            ->mutateBeforeCreate(fn ($value) => Carbon::parse($value))
+                            ->required(),
+                        ImportField::make('tingkat_pendidikan')
+                            ->required(),
+                        ImportField::make('pekerjaan')
+                            ->required(),
+                        ImportField::make('pendapatan')
+                            ->required(),
+                        ImportField::make('status_pernikahan')
+                            ->required(),
+                        ImportField::make('agama')
+                            ->required(),
+                        ImportField::make('kewarganegaraan')
+                            ->required(),
+                        ImportField::make('alamat')
+                            ->required(),
+                    ],
+                    columns: 2
+                )
         ];
     }
 }
